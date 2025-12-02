ddd62a68c2b5183c045a38a02387ddb4afa71338
Brachyy <fifoye06@gmail.com>
2025-12-02 20:17:37 +0100

    Fix: Remove duplicate imports in main.jsx causing build error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { AuthProvider } from './context/AuthContext';
 import { ListProvider } from './context/ListContext';
 import { SocialProvider } from './context/SocialContext';
